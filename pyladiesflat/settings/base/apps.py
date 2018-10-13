5dbd05e8b96344b7dffd339f5c2a2153edd1b6a7
Alla Zakharova <40774302+Mi5aka@users.noreply.github.com>
2018-10-13 14:28:29 +0300

    Dev (#2)
    * update settings files
    
    * update pipenv files

---
+++ pyladiesflat/settings/base/apps.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+
+    'pyladies.apps.PyladiesConfig',
+
+    'markdownx',
+    'markdownify'
+]
