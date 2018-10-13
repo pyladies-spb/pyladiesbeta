6f6ce1ac402853dc7eeb8954f634be3fb3e3de7c
zakharova_a <mi5akatwi@gmail.com>
2018-10-13 14:21:04 +0300

    update settings files

---
+++ pyladiesflat/settings/base/security.py
@@ -0,0 +1,38 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'aiadnx!w9-ad0p%yg$ddr!s+p=)++d_nm0#rs=v58xhoe16=(f'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+ROOT_URLCONF = 'pyladiesflat.urls'
+
+WSGI_APPLICATION = 'pyladiesflat.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+SITE_ID = 1
