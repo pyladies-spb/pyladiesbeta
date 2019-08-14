c6c6c5d130692c7ce2d89148394f20f642572929
Mi5aka <mi5akatwi@gmail.com>
2019-08-14 22:15:36 +0300

    update settings file; add models, views, urls, parts of templates

--- pyladies/views.py
+++ pyladies/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from pyladies.models import StoryPoint, Schedule, Publication, Resource, Partner
+
+
+def base_view(request):
+    story_points = StoryPoint.objects.all().order_by('date')
+    return render(request, 'about.html', {'story_points': story_points})
+
+
+def schedule_view(request):
+    schedule = Schedule.objects.all().order_by('-date')
+    return render(request, 'schedule.html', {'schedule': schedule})
+
+
+def publication_view(request):
+    publications = Publication.objects.all().order_by('-date')
+    return render(request, 'publications.html', {'publications': publications})
+
+
+def resource_view(request):
+    resources = Resource.objects.all().order_by('-order_number')
+    return render(request, 'resources.html', {'resources': resources})
+
+
+def partner_view(request):
+    partners = Publication.objects.all()
+    return render(request, 'partners.html', {'partners': partners})
+
--- pyladiesflat/settings/base/middleware.py
+++ pyladiesflat/settings/base/middleware.py
@@ -6,5 +6,4 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 ]
